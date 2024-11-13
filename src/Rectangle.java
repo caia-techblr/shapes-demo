583ba7e4079627f09fb2cb8bf8a09d7b0bdbf1db
Rajesh Sola <rsola32@gitam.edu>
2024-11-13 15:02:50 +0530

    updated reference code

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,5 +1,19 @@
 package org.gitam.cseblr;
 
 public class Rectangle extends Polygon {
-
+	int mLength;
+	int mBreadth;
+	Rectangle(int len,int br)
+	{
+		mLength = len;
+		mBreadth = br;
+	}
+	public double area()
+	{
+		return mLength * mBreadth;
+	}
+	public double circumference()
+	{
+		return 2 * (mLength + mBreadth);
+	}
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,5 +1,12 @@
 package org.gitam.cseblr;
 
 public class Triangle extends Polygon {
-
+	public double area()
+	{
+		return 0;
+	}
+	public double circumference()
+	{
+		return 0;
+	}
 }
